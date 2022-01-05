8d1f4887c974190739aec40c0e5bd5ff8b812bbe
Pedro Oliveira Amâncio <pedroamancio00@gmail.com>
2022-01-05 12:30:17 -0300

    feat: Apagar produtos [ATIVIDADE-01]

---
+++ src/products/use-cases/delete-product.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IDeleteProduct } from '../contracts/delete-product';
+
+import { ProductNotFoundException } from '../exceptions/product-not-found';
+import { ProductRepository } from '../products.repository';
+
+@Injectable()
+export class DeleteProduct implements IDeleteProduct {
+  constructor(
+    @InjectRepository(ProductRepository)
+    private productRepository: ProductRepository,
+  ) {}
+
+  async execute(params: IDeleteProduct.Params): IDeleteProduct.Response {
+    const product = await this.productRepository.findOne({
+      productId: params,
+    });
+    if (!product) throw new ProductNotFoundException();
+    await this.productRepository.softDelete(product.productId);
+    return { message: 'Product deleted' };
+  }
+}
